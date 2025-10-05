24fcb25aa3236227748e9475da29871dd2a82aef
Diviyaprakash <diviyaprakash32@gmail.com>
2025-10-05 10:06:58 +0530

    yes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -82,7 +82,7 @@ function Contact() {
     setStatus({ type: "", message: "" });
 
     try {
-      const response = await fetch("/api/contact", {
+      const response = await fetch("/.netlify/functions/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
